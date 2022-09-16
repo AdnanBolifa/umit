879f6c8dc1036a5e2e31b23534a3ad7cbeafc7e2
TheF0x <retr0f0x.f0@gmail.com>
2022-09-16 15:20:00 +0200

    refactor

--- Login page/index.js
+++ Login page/index.js
@@ -1,59 +1,94 @@
-const indicator = document.querySelector(".indicator");
-         const input = document.querySelector("input");
-         const weak = document.querySelector(".weak");
-         const medium = document.querySelector(".medium");
-         const strong = document.querySelector(".strong");
-         const text = document.querySelector(".text");
-         const showBtn = document.querySelector(".showBtn");
-         let regExpWeak = /[a-z]/;
-         let regExpMedium = /\d+/;
-         let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
-         function trigger(){
-           if(input.value != ""){
-             indicator.style.display = "block";
-             indicator.style.display = "flex";
-             if(input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong)))no=1;
-             if(input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong))))no=2;
-             if(input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong))no=3;
-             if(no==1){
-               weak.classList.add("active");
-               text.style.display = "block";
-               text.textContent = "Your password is too week";
-               text.classList.add("weak");
-             }
-             if(no==2){
-               medium.classList.add("active");
-               text.textContent = "Your password is medium";
-               text.classList.add("medium");
-             }else{
-               medium.classList.remove("active");
-               text.classList.remove("medium");
-             }
-             if(no==3){
-               weak.classList.add("active");
-               medium.classList.add("active");
-               strong.classList.add("active");
-               text.textContent = "Your password is strong";
-               text.classList.add("strong");
-             }else{
-               strong.classList.remove("active");
-               text.classList.remove("strong");
-             }
-             showBtn.style.display = "block";
-             showBtn.onclick = function(){
-               if(input.type == "password"){
-                 input.type = "text";
-                 showBtn.textContent = "HIDE";
-                 showBtn.style.color = "#23ad5c";
-               }else{
-                 input.type = "password";
-                 showBtn.textContent = "SHOW";
-                 showBtn.style.color = "#000";
-               }
-             }
-           }else{
-             indicator.style.display = "none";
-             text.style.display = "none";
-             showBtn.style.display = "none";
-           }
-         }
+var myInput=document.getElementById("psw");
+var letter=document.getElementById("letter");
+var capital=document.getElementById("capital");
+var number=document.getElementById("number");
+var specialChar=document.getElementById("specialChar");
+var length=document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus=function()
+{
+    document.getElementById("message").style.display="block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur=function()
+{
+    document.getElementById("message").style.display="none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup=function()
+{
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/;
+
+    if(myInput.value.match(lowerCaseLetters))
+    {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    }
+
+    else
+    {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/;
+
+    if(myInput.value.match(upperCaseLetters))
+    {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    }
+
+    else
+    {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/;
+
+    if(myInput.value.match(numbers))
+    {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    }
+
+    else
+    {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if(myInput.value.length >= 8)
+    {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    }
+
+    else
+    {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+
+    var specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if(myInput.value.match(specialCharacters))
+    {
+        specialChar.classList.remove("invalid");
+        specialChar.classList.add("valid");
+    }
+
+    else
+    {
+        specialChar.classList.remove("valid");
+        specialChar.classList.add("invalid");
+    }
+    
+}
